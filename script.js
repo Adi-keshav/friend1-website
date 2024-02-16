acab46c357e1828aef71eb2f9db1635e50a6d780
Aditya <156400354+Adi-keshav@users.noreply.github.com>
2024-02-16 09:18:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I'm so thankful for this Friendship😍";
+  }
+  function myFunction2() {
+  document.getElementById("demo").innerHTML = "Go to hell😑😡";
+  }
